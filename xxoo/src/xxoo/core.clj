12a05c1d36c01cb0942aa6bca5c1d13fc3b421f0
heganjie <heganjie@gmail.com>
2015-08-05 18:06:16 +0800

    xxoo: handle click event

--- xxoo/src/xxoo/core.clj
+++ xxoo/src/xxoo/core.clj
@@ -3,15 +3,18 @@
   (:gen-class))
 
 (defn -main [& args]
-  (invoke-later
-    (let [f (frame :title "Game",
-                   :size [300 :by 300]
-                   :content (grid-panel
-                              :columns 3
-                              :items (repeatedly 9 button)),
-                   :on-close :dispose)]
-      (show! f)
-      (listen (select f [:JButton]) :action (fn [e] (println e))))))
+  )
+
+(invoke-later
+  (let [f (frame :title "Game",
+                 :size [300 :by 300]
+                 :content (grid-panel
+                            :columns 3
+                            :items (repeatedly 9 button)),
+                 :on-close :dispose)]
+    (.setLocationRelativeTo f nil)
+    (show! f)
+    (listen (select f [:JButton]) :mouse-clicked (fn [e] (config! e :text "x")))))
 
 
 
